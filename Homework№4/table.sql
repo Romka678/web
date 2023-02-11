6990bb226675f3b609387b43f9b57372aaa858c8
Роман Вятчанин <viat4anin@gmail.com>
2023-02-11 15:48:39 +0300

    Done Homework 4

---
+++ Homework№4/table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE classmates (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  address TEXT NOT NULL
+);
+
+INSERT INTO classmates VALUES (1, 'Иван', 21, 'Омск');
+INSERT INTO classmates VALUES (2, 'Петя', 30, 'Казань');
+INSERT INTO classmates VALUES (3, 'Семен', 33, 'Москва');
+INSERT INTO classmates VALUES (4, 'Маша', 19, 'Волгоград');
+INSERT INTO classmates VALUES (5, 'Дима', 18, 'Пермь');
+INSERT INTO classmates VALUES (6, 'Леша', 32, 'Петрозаводск');
+INSERT INTO classmates VALUES (7, 'Максим', 19, 'Севастополь');
